687ae6a0b53c95898135dee481ad12a9d74dfb7d
Olivier Houte <github@houte.com>
2026-09-20 16:41:32 +0200

    refactor: share native rendering primitives without changing production behavior

--- internal/graphics/renderer.go
+++ internal/graphics/renderer.go
@@ -1,7 +1,7 @@
 package graphics
 
 import (
-	"encoding/binary"
+	"github.com/olivierh59500/democonstructionkit/indexed"
 	"image"
 	"image/color"
 	"sync"
@@ -211,10 +211,5 @@ func modePixelCount(mode Mode) (int, bool) {
 }
 
 func fillFramePixels(dst, indices []byte, palette *[constants.PaletteColorCount]uint32) {
-	if len(dst) < len(indices)*4 {
-		return
-	}
-	for i, index := range indices {
-		binary.LittleEndian.PutUint32(dst[i*4:], palette[index])
-	}
+	_ = indexed.ExpandRGBA(dst, indices, palette)
 }
